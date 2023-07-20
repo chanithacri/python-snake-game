6aa4851cd2b8bfd365a27b3520cb616f27701909
chanithacri <113086455+chanithacri@users.noreply.github.com>
2023-07-20 16:39:23 +0530

    Add files via upload

---
+++ snake_game.py
@@ -0,0 +1,176 @@
+import turtle
+import time
+import random
+
+delay = 0.07
+
+# Score
+score = 0
+
+# create screen
+screen = turtle.Screen()
+screen.title('Snake Game')
+screen.bgcolor('black')
+screen.setup(width=600, height=600)
+screen.tracer(0) # Turns off the screen updates
+
+# snake head
+head = turtle.Turtle()
+head.shape('square')
+head.color('white')
+head.penup()
+head.goto(0, 0)
+head.direction = 'stop'
+
+# food
+food = turtle.Turtle()
+food.speed(0)
+food.shape('circle')
+food.color('red')
+food.penup()
+food.goto(0, 100)
+
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0", align="center", font=("Courier", 24, "normal"))
+
+# functions
+
+
+def go_up():
+    if head.direction != 'down':
+        head.direction = 'up'
+
+
+def go_down():
+    if head.direction != 'up':
+        head.direction = 'down'
+
+
+def go_left():
+    if head.direction != 'right':
+        head.direction = 'left'
+
+
+def go_right():
+    if head.direction != 'left':
+        head.direction = 'right'
+
+
+def move():
+    if head.direction == 'up':
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == 'down':
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == 'left':
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == 'right':
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+# keyboard bindings
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+screen.onkey(go_left, "Left")
+screen.onkey(go_right, "Right")
+
+# main game
+try:
+    while True:
+        screen.update()
+
+        # Collision detect with border
+        if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+            head.goto(0,0)
+            head.direction = "Stop"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # Clean the segments list
+            segments.clear()
+
+            # Reset score
+            score = 0
+
+            # Reset delay
+            delay = 0.07
+
+            pen.clear()
+            pen.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+        # Check for collision with food
+        if head.distance(food) < 20:
+            x = random.randint(-290, 290)
+            y = random.randint(-290, 290)
+            food.goto(x,y)
+
+            # add a segment to snake
+            new_segment = turtle.Turtle()
+            new_segment.speed(0)
+            new_segment.shape('square')
+            new_segment.color('grey')
+            new_segment.penup()
+            segments.append(new_segment)
+
+            # shorten the delay
+            delay = 0.07
+
+            # Increase the score
+            score += 10
+
+            pen.clear()
+            pen.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+        for index in range(len(segments) - 1, 0, -1):
+            x = segments[index - 1].xcor()
+            y = segments[index - 1].ycor()
+            segments[index].goto(x, y)
+
+        if len(segments) > 0:
+            x = head.xcor()
+            y = head.ycor()
+            segments[0].goto(x, y)
+
+        move()
+
+        for segment in segments:
+            if segment.distance(head) < 20:
+                time.sleep(1)
+                head.goto(0, 0)
+                head.direction = "Stop"
+
+                for segment in segments:
+                    segment.goto(1000, 1000)
+
+                segments.clear()
+
+                # Reset score
+                score = 0
+
+                # Reset delay
+                delay = 0.07
+
+                pen.clear()
+                pen.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+        time.sleep(delay)
+except turtle.Terminator:
+    print("Game Over")
+
+screen.mainloop()
